35155e082cf971f46048a7e151ef32daab5bb718
ramyadav10 <ramyadav.now@gmail.com>
2021-09-22 08:52:35 +0530

    Creating the Address Book with Rest Controller,ResponseDto,ResponseEntity

--- src/main/java/com/mastercode/abs/controller/AddressBookController.java
+++ src/main/java/com/mastercode/abs/controller/AddressBookController.java
@@ -1,8 +1,11 @@
 package com.mastercode.abs.controller;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mastercode.abs.dto.ResponseDTO;
 import com.mastercode.abs.service.IAddressBookService;
+
 
 public class AddressBookController {
 
@@ -24,29 +29,32 @@ public class AddressBookController {
 	@GetMapping("/retrive")
 	public ResponseEntity<ResponseDTO> getAllData()
 	{
-		
+		return (new ResponseEntity(dto,HttpStatus.OK));
 	}
 	
 	@GetMapping("/retrive/{id}")
 	public ResponseEntity<ResponseDTO> findById(@PathVariable int id)
 	{
-			}
+		return (new ResponseEntity(dto,HttpStatus.OK));
+	}
 	
 	
 	@PostMapping("/insert") 
-	public ResponseEntity<ResponseDTO> addRecord( @RequestBody AddressBookDTO addressbookdto) //throws CustomException
+	public ResponseEntity<ResponseDTO> addRecord(@Valid @RequestBody AddressBookDTO addressbookdto) //throws CustomException
 	{
+			return (new ResponseEntity(dto,HttpStatus.CREATED));
 	}
 	
 	
 	@PutMapping("/update/{id}")
-	public ResponseEntity<ResponseDTO> updateRecords( @RequestBody AddressBookDTO addressbookdto,@PathVariable int id)
+	public ResponseEntity<ResponseDTO> updateRecords(@RequestBody AddressBookDTO addressbookdto,@PathVariable int id)
 	{
+		return (new ResponseEntity(dto,HttpStatus.OK));
 	}
 	
-	@RequestMapping("/delete/{id}")
+		@RequestMapping("/delete/{id}")
 	public ResponseEntity<ResponseDTO> deleteRecordById(@PathVariable int id)
 	{
+		return (new ResponseEntity(dto,HttpStatus.OK));
 	}
-
 }
---
+++ src/main/java/com/mastercode/abs/dto/ResponseDTO.java
@@ -0,0 +1,15 @@
+package com.mastercode.abs.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseDTO {
+
+	private String message;
+	private Object data;
+	
+	public ResponseDTO(String message, Object data) {
+		this.message = message;
+		this.data = data;
+	}
+}
